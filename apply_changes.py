fd0a9119f5a37cbdbbb55094c6db31a6f72eebcb
Devender Mishra <devender.mishra@ril.com>
2015-08-13 14:44:50 +0000

    Add apply_changes

---
+++ apply_changes.py
@@ -0,0 +1,18 @@
+import sys
+import yaml
+from subprocess import call
+
+infile = sys.argv[1]
+inventory = sys.argv[2]
+username = sys.argv[3]
+limited_host = ""
+if len(sys.argv) >= 5:
+  limited_host = sys.argv[4]
+
+call_list = ["ansible-playbook", "-i", inventory, "make_changes.yml", "-u", username, "--extra-vars", "varfile="+infile]
+if limited_host:
+  call_list.extend(["-l", limited_host])
+
+#print "Argument: "+" ".join(call_list)
+call(call_list)
+
